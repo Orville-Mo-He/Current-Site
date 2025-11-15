26c81dfa7c32c36bb353582486cd900d3c91b2ff
Orville-Mo-He <81935112+Orville-Mo-He@users.noreply.github.com>
2025-11-14 19:14:23 -0500

    directory cleanup + css

--- black-site script.js
+++
@@ -1,7 +0,0 @@
-let root = document.documentElement;
-let rem = window.getComputedStyle(root, null).getPropertyValue("--base-font-size");
-console.log(rem);
-
-let body = document.body;
-let width = body.clientWidth;
-console.log(width);
