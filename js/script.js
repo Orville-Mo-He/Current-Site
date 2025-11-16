28a8dc220ae04712d924c2a2004d5ea940500fd3
Orville-Mo-He <81935112+Orville-Mo-He@users.noreply.github.com>
2025-11-16 18:04:54 -0500

    nav done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ console.log(rem);
 let body = document.body;
 let width = body.clientWidth;
 console.log(width);
+
+
+// Mobile Nav
+const openButton = document.getElementById('open-mobile-nav');
+const closeButton = document.getElementById('close-mobile-nav');
+const mobileNav = document.getElementById('mobile-nav');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav-links a');
+
+// Open menu
+openButton.addEventListener('click', function () {
+    mobileNav.classList.add('active');
+    document.body.classList.add('no-scroll');
+});
+
+// Close menu with close button
+closeButton.addEventListener('click', function () {
+    mobileNav.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+});
+
+// Close menu when clicking nav links
+mobileNavLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        mobileNav.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    });
+});
